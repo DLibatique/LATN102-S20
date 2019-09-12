37f8e02eaeaa3b506b865e0b302677e862640bb6
Daniel Libatique <daniel.libatique@gmail.com>
2019-09-12 12:17:59 -0400

    added adj-noun pair charts, added toggle for rows of charts

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -2,3 +2,11 @@ function toggleDisplay(id) {
   const el = document.getElementById(id);
   el.style.display = el.style.display === 'none' ? 'inline' : 'none';
 }
+
+function toggleDisplayChart(x) {
+  const el = document.getElementsByClassName(x);
+
+  for (var i = 0; i < el.length; i++){
+          el[i].style.display = el[i].style.display === 'none' ? 'table-row-group' : 'none';
+      }
+}
